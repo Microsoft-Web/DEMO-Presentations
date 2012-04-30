32d89e8de37f18cc4c44744117d46c7d4995d4e6
brady gaster <brady.gaster@gmail.com>
2012-04-29 19:36:58 -0700

    added bruno t signalr demo

---
+++ SignalRShapeDemo/SignalRShapeDemo/MoveShape.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+// Add reference
+using SignalR.Hubs;
+
+
+namespace CloudSignalRSample_WebRole
+{
+    // Derive from Hub, which is a server-side class
+    // and a client side proxy.
+    [HubName("moveShape")]  // moveShape is the name used in the Javascript
+    public class MoveShapeHub : Hub
+    {
+        public void MoveShape(int x, int y)
+        {
+            // Broadcast to clients to move shape
+            Clients.shapeMoved(Context.ConnectionId, x, y);
+
+            // Simple diagnostics for debugging
+            System.Diagnostics.Debug.WriteLine("x = " + x + ", y = " + y);
+        }
+    }
+}
---
+++ SignalRShapeDemo/SignalRShapeDemo/MoveShape.js
@@ -0,0 +1,33 @@
+﻿/// <reference path="Scripts/jquery-1.6.4.js" />
+/// <reference path="Scripts/jquery.signalR.js" />
+/// <reference path="Scripts/jquery-ui-1.8.18.js" />
+
+$(function () {
+    // Get a refeerence to the server-side moveShape() class.
+    var hub = $.connection.moveShape,
+    // Get a reference to the shape div in the html
+    $shape = $("#shape");
+    // Use extend to move the shape object (if we are not the sender)
+    $.extend(hub, {
+
+        // Use css to move the shape object
+
+        shapeMoved: function (cid, x, y) {
+            if ($.connection.hub.id !== cid) {
+                $shape.css({ left: x, top: y });
+                $("p:last").text("left: " + x + ", top: " + y);
+            }
+        }
+    });
+    // Wire up the draggable behavior (when hub is done starting)
+    // "done" is a jquery deferred method
+    $.connection.hub.start().done(function () {
+        $shape.draggable({
+            // Implement draggable effect for jquery
+            drag: function () {
+                // Tell the server that the shape was just dragged
+                hub.moveShape(this.offsetLeft, this.offsetTop);
+            }
+        });
+    })
+});
