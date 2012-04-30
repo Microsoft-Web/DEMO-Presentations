2d09ffb3759e4402bafcc46cf51a4de7b2152d06
brady gaster <brady.gaster@gmail.com>
2012-04-29 20:09:21 -0700

    added nancy demo

---
+++ NancyDemo/NancyDemo/HelloUserModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace NancyDemo
+{
+    public class HelloUserModule : NancyModule
+    {
+        public HelloUserModule()
+        {
+            Get["/{name}"] = parameters => "Hello " + parameters.name;
+        }
+    }
+}
